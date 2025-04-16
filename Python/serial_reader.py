a2bfad4e3ac2fa6fba2de0ffe2029f6bb544bba6
amey <bhadaneamey@gmail.com>
2025-04-16 20:46:29 +0530

    Update comments

--- Python/serial_reader.py
+++ Python/serial_reader.py
@@ -1,7 +1,21 @@
 import serial
 import time
 
+<<<<<<< HEAD
 # Configure details of Serial() as per the COM port or USB/tty port on your machine through which serial data is being transmitted.
+=======
+'''
+In case of Linux:
+
+ser = serial.Serial(
+	port='/dev/ttyUSB0',
+	baudrate=9600,
+	parity=serial.PARITY_NONE,
+	stopbits=serial.STOPBITS_ONE,
+	bytesize=serial.EIGHTBITS,
+	timeout=1)
+'''
+>>>>>>> 81fd9d6 (Update serial_reader.py)
 
 def read_serial_data(port='COM3', baudrate=9600, timeout=3):
     try:
