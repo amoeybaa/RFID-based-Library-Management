3fd9d57d6b55ed2385320a0216f0a1512f93c570
Amey <bhadaneamey@gmail.com>
2025-04-16 16:52:22 +0530

    Update app.py

--- Python/app.py
+++ Python/app.py
@@ -10,36 +10,3 @@ def index():
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-
-'''
-from flask import Flask, render_template
-import time
-import serial
-
-app = Flask(__name__)
-
-def read_serial_data():
-    ser = serial.Serial('COM3', 9600, timeout=3)
-    data = ser.readline().strip().decode()
-    if(data):
-        time.sleep(0.5)
-        print(data)
-        return data
-    else:
-        time.sleep(0.5)
-        print('0')
-        return '0'
-
-@app.route('/')
-def index():
-    while(True):
-        serial_data = read_serial_data()
-        if(serial_data == '0'):
-            return render_template('index.html')
-        else:
-            return render_template('index.html', serial_data=serial_data)
-
-if __name__ == '__main__':
-    app.run(debug=True)
-'''
